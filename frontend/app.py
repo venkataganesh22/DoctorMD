62f948254a0ad7d7e4b533f7997c9a9c33ed049f
venkataganesh22 <venkataganeshguthi@gmail.com>
2026-08-10 22:12:03 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -11,7 +11,7 @@ st.set_page_config(
     layout="centered"
 )
 
-API_URL = "http://127.0.0.1:8000"
+API_URL = "https://doctormd.onrender.com"
 
 
 # --------------------------------------------------
